def632eaad93ee6592848d2db4abb586fccafea1
myeonghoon <mhoo999@naver.com>
2024-10-31 21:53:40 +0900

    완료
    완료

--- Source/AutoWidgetSwitcher.Build.cs
+++ Source/AutoWidgetSwitcher.Build.cs
@@ -8,7 +8,7 @@ public class AutoWidgetSwitcher : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "Slate", "SlateCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
